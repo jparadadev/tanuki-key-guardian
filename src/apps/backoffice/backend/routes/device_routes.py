d9361032a887165d42d3650e4b5211711fdcdfc7
Javier Parada <javierparada73@outlook.com>
2022-03-09 18:53:53 +0100

    add open api docs

--- src/apps/backoffice/backend/routes/device_routes.py
+++ src/apps/backoffice/backend/routes/device_routes.py
@@ -2,6 +2,8 @@ import sys
 
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from src.apps.backoffice.backend.controllers.DevicesGetController import DevicesGetController
 from src.apps.backoffice.backend.controllers.DevicePostController import DevicePostController
@@ -14,8 +16,13 @@ def register(
         devices_get_controller: DevicesGetController = Provide[BackofficeContainer.devices_get_controller],
         device_post_controller: DevicePostController = Provide[BackofficeContainer.device_post_controller],
 ):
-    router.add_route('/devices', devices_get_controller.run)
-    router.add_route('/devices', device_post_controller.run, ['POST'])
+    @router.post('/devices', tags=["Devices"])
+    async def run_wrapper(req: Request) -> JSONResponse:
+        return await device_post_controller.run(req)
+
+    @router.get('/devices', tags=["Devices"])
+    async def run_wrapper(req: Request) -> JSONResponse:
+        return await devices_get_controller.run(req)
 
 
 backoffice_container.wire(modules=[sys.modules[__name__]])
--- src/apps/backoffice/backend/routes/status_routes.py
+++ src/apps/backoffice/backend/routes/status_routes.py
@@ -2,6 +2,7 @@ import sys
 
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter
+from starlette.requests import Request
 
 from src.apps.backoffice.backend.controllers.StatusGetController import StatusGetController
 from src.apps.backoffice.backend.dependencies.BackofficeContainer import BackofficeContainer, backoffice_container
@@ -12,7 +13,9 @@ def register(
         router: APIRouter,
         status_get_controller: StatusGetController = Provide[BackofficeContainer.status_get_controller]
 ):
-    router.add_route('/status', status_get_controller.run)
+    @router.get('/status', tags=["Health"])
+    async def run_wrapper(req: Request):
+        return await status_get_controller.run(req)
 
 
 backoffice_container.wire(modules=[sys.modules[__name__]])
