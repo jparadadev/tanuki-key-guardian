c4adb6204697b81e5470c2ca8c06146066dfd96e
Javier Parada <javierparada73@outlook.com>
2022-03-09 15:45:41 +0100

    Add date value object to device

--- src/contexts/backoffice/device/domain/entities/Device.py
+++ src/contexts/backoffice/device/domain/entities/Device.py
@@ -1,21 +1,25 @@
+from datetime import datetime
 from typing import Dict, List, Union, Any
 
 from src.contexts.backoffice.device.domain.domainevents.DeviceCreatedDomainEvent import DeviceCreatedDomainEvent
-from src.contexts.backoffice.device.domain.entities.DeviceCreationDate import DeviceName
+from src.contexts.backoffice.device.domain.entities.DeviceCreationDate import DeviceCreationDate
 from src.contexts.backoffice.device.domain.entities.DeviceId import DeviceId
+from src.contexts.backoffice.device.domain.entities.DeviceName import DeviceName
 from src.contexts.shared.domain.valueobj.AggregateRoot import AggregateRoot
 
 
 class Device(AggregateRoot):
 
-    def __init__(self, device_id: DeviceId, name: DeviceName):
+    def __init__(self, device_id: DeviceId, name: DeviceName, created_at: DeviceCreationDate):
         super().__init__()
         self.id = device_id
         self.name = name
+        self.created_at = created_at
 
     @staticmethod
     def create(device_id: DeviceId, name: DeviceName):
-        device = Device(device_id, name)
+        now = DeviceCreationDate(datetime.now())
+        device = Device(device_id, name, now)
         event = DeviceCreatedDomainEvent(device.id.value(), device)
         device.record_event(event)
         return device
@@ -25,6 +29,7 @@ class Device(AggregateRoot):
         device = Device(
             DeviceId(raw_data.get('id')),
             DeviceName(raw_data.get('name')),
+            DeviceCreationDate(raw_data.get('created-at')),
         )
         return device
 
@@ -32,4 +37,5 @@ class Device(AggregateRoot):
         return {
             'id': self.id.value(),
             'name': self.name.value(),
+            'created-at': self.created_at.value(),
         }
--- src/contexts/backoffice/device/domain/entities/DeviceCreationDate.py
+++ src/contexts/backoffice/device/domain/entities/DeviceCreationDate.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from src.contexts.shared.domain.valueobj.ValueObject import ValueObject
 
 
-class DeviceName(ValueObject):
+class DeviceCreationDate(ValueObject):
 
     def __init__(self, value: datetime):
         super().__init__(value)
