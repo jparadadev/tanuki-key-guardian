ccbcabbed147384e6c023b232b0d1beda970d0d8
Javier Parada <javierparada73@outlook.com>
2022-03-10 19:59:12 +0100

    register routes

--- src/apps/backoffice/backend/routes/__init__.py
+++ src/apps/backoffice/backend/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from src.apps.backoffice.backend.routes.status_routes import register as register_status_routes
 from src.apps.backoffice.backend.routes.client_routes import register as register_client_routes
+from src.apps.backoffice.backend.routes.cryptokey_routes import register as register_cryptokey_routes
 
 
 def register_routes(router: APIRouter):
     register_status_routes(router)
     register_client_routes(router)
+    register_cryptokey_routes(router)
