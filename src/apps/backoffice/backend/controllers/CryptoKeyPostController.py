45a8be5f26eed65c9050a0e418996dfea385dbae
Javier Parada <javierparada73@outlook.com>
2022-03-10 19:52:35 +0100

    Add controllers and dependency injection

---
+++ src/apps/backoffice/backend/controllers/CryptoKeyPostController.py
@@ -0,0 +1,33 @@
+from typing import Dict, Any
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from http import HTTPStatus
+
+from src.apps.backoffice.backend.controllers.BackofficeController import BackofficeController
+from src.contexts.backoffice.cryptokeys.application.create_one.CreateCryptoKeyCommand import CreateCryptoKeyCommand
+from src.contexts.backoffice.cryptokeys.infrastructure.CryptoKeysHttpResponseErrorHandler import \
+    JsonResponseErrorHandler
+from src.contexts.shared.domain.CommandBus import CommandBus
+from src.contexts.shared.domain.errors.DomainError import DomainError
+
+
+class CryptoKeyPostController(BackofficeController):
+
+    def __init__(
+            self,
+            command_bus: CommandBus,
+    ):
+        self._command_bus = command_bus
+        self._error_handler = JsonResponseErrorHandler()
+
+    async def run(self, req: Request) -> JSONResponse:
+        body: Dict[str, Any] = await req.json()
+        command: CreateCryptoKeyCommand = CreateCryptoKeyCommand(body.get('id'), body.get('client-id'),
+                                                                 body.get('type'))
+        try:
+            await self._command_bus.dispatch(command)
+        except DomainError as err:
+            return self._error_handler.resolve(err)
+
+        return JSONResponse(status_code=HTTPStatus.CREATED)
--- src/apps/backoffice/backend/dependencies/BackofficeContainer.py
+++ src/apps/backoffice/backend/dependencies/BackofficeContainer.py
@@ -1,5 +1,7 @@
 from dependency_injector import containers, providers
 
+from src.apps.backoffice.backend.controllers.CryptoKeyPostController import CryptoKeyPostController
+from src.apps.backoffice.backend.controllers.CryptoKeysGetController import CryptoKeysGetController
 from src.apps.backoffice.backend.controllers.StatusGetController import StatusGetController
 from src.apps.backoffice.backend.controllers.ClientsGetController import ClientsGetController
 from src.apps.backoffice.backend.controllers.ClientPostController import ClientPostController
@@ -11,6 +13,14 @@ from src.contexts.backoffice.clients.application.findall.FindClientsByCriteriaQu
 from src.contexts.backoffice.clients.infrastructure.persistence.PyMongoClientRepository import PyMongoClientRepository
 from src.contexts.backoffice.clients.infrastructure.persistence.config.PyMongoClientConfigFactory import \
     PyMongoClientConfigFactory
+from src.contexts.backoffice.cryptokeys.application.create_one.CreateCryptoKeyCommandHandler import \
+    CreateCryptoKeyCommandHandler
+from src.contexts.backoffice.cryptokeys.application.create_one.CryptoKeyCreator import CryptoKeyCreator
+from src.contexts.backoffice.cryptokeys.application.findall.CryptoKeysByCriteriaFinder import CryptoKeysByCriteriaFinder
+from src.contexts.backoffice.cryptokeys.application.findall.FindCryptoKeysByCriteriaQueryHandler import \
+    FindCryptoKeysByCriteriaQueryHandler
+from src.contexts.backoffice.cryptokeys.infrastructure.persistence.PyMongoCryptoKeyRepository import \
+    PyMongoCryptoKeyRepository
 from src.contexts.shared.Infrastructure.commandbus.InMemoryCommandBus import InMemoryCommandBus
 from src.contexts.shared.Infrastructure.eventbus.InMemoryEventBus import InMemoryEventBus
 from src.contexts.shared.Infrastructure.persistence.mongo.PyMongoClientFactory import PyMongoClientFactory
@@ -27,11 +37,18 @@ class BackofficeContainer(containers.DeclarativeContainer):
     db_client = providers.Singleton(PyMongoClientFactory.create_instance, 'backoffice', db_config)
 
     client_repository = providers.Singleton(PyMongoClientRepository, db_client)
+    cryptokey_repository = providers.Singleton(PyMongoCryptoKeyRepository, db_client)
 
     clients_by_criteria_finder = providers.Singleton(ClientsByCriteriaFinder, client_repository)
     find_clients_by_criteria_query_handler = providers.Singleton(
         FindClientsByCriteriaQueryHandler,
         clients_by_criteria_finder,
+    )
+
+    cryptokeys_by_criteria_finder = providers.Singleton(CryptoKeysByCriteriaFinder, cryptokey_repository)
+    find_cryptokeys_by_criteria_query_handler = providers.Singleton(
+        FindCryptoKeysByCriteriaQueryHandler,
+        cryptokeys_by_criteria_finder,
     )
 
     client_creator = providers.Singleton(ClientCreator, client_repository, event_bus)
@@ -40,19 +57,29 @@ class BackofficeContainer(containers.DeclarativeContainer):
         client_creator,
     )
 
+    cryptokey_creator = providers.Singleton(CryptoKeyCreator, cryptokey_repository, event_bus)
+    create_cryptokey_command_handler = providers.Singleton(
+        CreateCryptoKeyCommandHandler,
+        cryptokey_creator,
+    )
+
     query_bus = providers.Singleton(
         InMemoryQueryBus,
         find_clients_by_criteria_query_handler,
+        find_cryptokeys_by_criteria_query_handler,
     )
 
     command_bus = providers.Singleton(
         InMemoryCommandBus,
         create_client_command_handler,
+        create_cryptokey_command_handler,
     )
 
     status_get_controller = providers.Singleton(StatusGetController)
     clients_get_controller = providers.Singleton(ClientsGetController, query_bus)
     client_post_controller = providers.Singleton(ClientPostController, command_bus)
+    cryptokeys_get_controller = providers.Singleton(CryptoKeysGetController, query_bus)
+    cryptokeys_post_controller = providers.Singleton(CryptoKeyPostController, command_bus)
 
 
 backoffice_container: BackofficeContainer = BackofficeContainer()
