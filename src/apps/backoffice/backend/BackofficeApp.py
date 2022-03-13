a1955efcde9e03e332325efa24ff8edec0658a0c
Javier Parada <javierparada73@outlook.com>
2022-03-13 12:13:46 +0100

    update openapi data

--- src/apps/backoffice/backend/BackofficeApp.py
+++ src/apps/backoffice/backend/BackofficeApp.py
@@ -6,7 +6,10 @@ from src.apps.backoffice.backend.routes import register_routes
 class BackofficeApp:
 
     def __init__(self):
-        self._app: FastAPI = FastAPI()
+        self._app: FastAPI = FastAPI(
+            title="Tanuki Key Guardian - Backoffice",
+            license_info={'url': 'https://www.apache.org/licenses/LICENSE-2.0.html', 'name': 'Apache 2.0'}
+        )
         router: APIRouter = APIRouter()
         register_routes(router)
         self._app.include_router(router, prefix='/api')
