43b0a6aaae2d561fcb203b88d070a95793fc9bf9
Javier Parada <javierparada73@outlook.com>
2022-03-15 17:46:18 +0100

    Update entrypoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
 
 from src.apps.backoffice.backend.boot import boot as boot_backoffice
+from src.apps.ca.boot import boot as boot_ca
 
 
 service_mapping = {
     'backoffice': boot_backoffice,
+    'ca': boot_ca,
 }
 
 if __name__ == "__main__":
@@ -13,7 +15,7 @@ if __name__ == "__main__":
         '--service',
         type=str,
         nargs='?',
-        help='Service to run must be one of ["backoffice"]',
+        help='Service to run must be one of ["backoffice", "ca"]',
     )
     params = vars(parser.parse_args())
     service_name = params['service']
