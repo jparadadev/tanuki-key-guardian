e498a1a09dd554c3c5b83d51dfb27db44aa28ed7
Javier Parada <javierparada73@outlook.com>
2022-03-20 12:00:07 +0100

    add values

--- src/contexts/kms/computed_data/domain/entities/ComputedDataType.py
+++ src/contexts/kms/computed_data/domain/entities/ComputedDataType.py
@@ -1,4 +1,21 @@
+from enum import Enum
+
 from src.contexts.shared.domain.valueobj.ValueObject import ValueObject
+
+
+class ComputedDataTypes(Enum):
+
+    ENCRYPT = 'encrypt'
+    DECRYPT = 'decrypt'
+
+    @classmethod
+    def has_value(cls, value):
+        values = [item.value for item in ComputedDataTypes]
+        return value in values
+
+    @classmethod
+    def get_values(cls):
+        return [item.value for item in ComputedDataTypes]
 
 
 class ComputedDataType(ValueObject):
