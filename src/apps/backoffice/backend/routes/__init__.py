9bb6d2b1bdcc5f5467b23c47070bd3c399efd6ff
Javier Parada <javierparada73@outlook.com>
2022-03-25 19:30:53 +0100

    add key generator to backoffice

--- src/apps/backoffice/backend/routes/__init__.py
+++ src/apps/backoffice/backend/routes/__init__.py
@@ -4,6 +4,7 @@ from src.apps.backoffice.backend.routes.status_routes import register as registe
 from src.apps.backoffice.backend.routes.client_routes import register as register_client_routes
 from src.apps.backoffice.backend.routes.cryptokey_routes import register as register_cryptokey_routes
 from src.apps.backoffice.backend.routes.computed_data_routes import register as register_computed_data_routes
+from src.apps.kms.routes.key_routes import register as register_key_routes
 
 
 def register_routes(router: APIRouter):
@@ -11,3 +12,4 @@ def register_routes(router: APIRouter):
     register_client_routes(router)
     register_cryptokey_routes(router)
     register_computed_data_routes(router)
+    register_key_routes(router)
