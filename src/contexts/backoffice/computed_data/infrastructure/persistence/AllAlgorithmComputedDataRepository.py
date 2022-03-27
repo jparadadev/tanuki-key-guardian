19d09f9f76d5dcaf9b4d085ac0096c4b398f0b69
Javier Parada <javierparada73@outlook.com>
2022-03-27 11:42:34 +0200

    add DH w hmac algorithm

--- src/contexts/backoffice/computed_data/infrastructure/persistence/AllAlgorithmComputedDataRepository.py
+++ src/contexts/backoffice/computed_data/infrastructure/persistence/AllAlgorithmComputedDataRepository.py
@@ -1,9 +1,11 @@
-from cryptography.hazmat.primitives import hashes, serialization
+import sys
+
+from cryptography.hazmat.primitives import hashes, serialization, hmac
 from cryptography.hazmat.primitives._serialization import PublicFormat, Encoding
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
-
+from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_parameters
 
 from src.contexts.backoffice.cryptokeys.domain.entities.CryptoKey import CryptoKey
 from src.contexts.backoffice.cryptokeys.domain.entities.CryptoKeyType import CryptoKeyTypes
@@ -23,6 +25,9 @@ class AllAlgorithmComputedDataRepository(BaseObject, ComputedDataRepository):
         if key.type.value() == CryptoKeyTypes.DIFFIE_HELLMAN.value:
             output = await self.ecdh_get_shared_key_platform(key.payload.value())
 
+        if key.type.value() == CryptoKeyTypes.DIFFIE_HELLMAN_HMAC.value:
+            output = await self.hmac_DH_GetSharedKey_Platform(key.parameters.value(), key.payload.value())
+
         data = ComputedData(
             input,
             ComputedDataOutput(output),
@@ -30,7 +35,6 @@ class AllAlgorithmComputedDataRepository(BaseObject, ComputedDataRepository):
             cd_type,
         )
         return data
-
 
     async def ecdh_get_shared_key_platform(self, public_key_IoT: str):
         # Input para shared key Platform: ID clave pública IoT
@@ -48,4 +52,41 @@ class AllAlgorithmComputedDataRepository(BaseObject, ComputedDataRepository):
         # Returns shared key platform
         shared_key = private_key.exchange(ec.ECDH(), loaded_public_key_IoT)
 
+        return shared_key.hex()
+
+    async def hmac_DH_GetSharedKey_Platform(self, dh_parameters: str, public_key_IoT: str, pk_signature: str):
+        # A la API le llega el id de la clave y de ahí saca el string de parámetros y de la public key del IoT
+
+        # En este punto, el KMS ya tiene los dh_parameters y la clave pública en string y debe convertirlo al objeto original
+        # 1. Conversión a bytes y deserialización de parámetros
+        dh_parameters_pem = dh_parameters.encode("utf-8")
+        loaded_dh_params = load_pem_parameters(dh_parameters_pem)
+
+        # 2. Conversión a bytes y deserialización de clave pública IoT
+        public_key_IoT_pem = public_key_IoT.encode('utf-8')
+        loaded_public_key_IoT = load_pem_public_key(public_key_IoT_pem)
+
+        # Comprueba que la clave pública del IoT es correcta
+        if (not isinstance(loaded_dh_params, dh.DHParameters) or not isinstance(loaded_public_key_IoT, dh.DHPublicKey)):
+            sys.exit('Protocol error: Platform received a wrong message!')
+
+        # Comprueba la autenticidad con la HMAC recibida
+        h_IoT = hmac.HMAC(public_key_IoT_pem, hashes.SHA256())
+        h_IoT.update(b"Platform public key hash")
+        h_IoT.verify(bytes.fromhex(pk_signature))
+
+        # 3. Plataforma genera sus propias claves privadas y públicas
+        platform_private_key = loaded_dh_params.generate_private_key()
+        platform_public_key = platform_private_key.public_key()  # El KMS debe guardar esta clave pública junto con los parámetros y str_signature en la bbdd con id platform-HMACDH
+        platform_pk_pem = platform_public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+        str_platform_pk = platform_pk_pem.decode('utf-8')
+
+        # HMAC
+        h = hmac.HMAC(platform_pk_pem, hashes.SHA256())
+        h.update(b"Platform public key hash")
+        signature = h.finalize()
+        str_signature = signature.hex()
+
+        shared_key = platform_private_key.exchange(loaded_public_key_IoT)
+        # Devuelve la clave en string, el IoT debe enviar al KMS luego de nuevo esta clave para que la almacene
         return shared_key.hex()
