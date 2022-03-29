62563a92f3c94c12bf6cdb57e22445f5affb3c28
Javier Parada <javierparada73@outlook.com>
2022-03-29 19:05:49 +0200

    fix http code status

--- src/apps/kms/backend/controllers/crypto_key/RotateCryptoKeyPutController.py
+++ src/apps/kms/backend/controllers/crypto_key/RotateCryptoKeyPutController.py
@@ -27,4 +27,4 @@ class RotateCryptoKeyPutController(KmsController):
         except DomainError as err:
             return self._error_handler.resolve(err)
 
-        return JSONResponse(status_code=HTTPStatus.CREATED)
+        return JSONResponse(status_code=HTTPStatus.ACCEPTED)
