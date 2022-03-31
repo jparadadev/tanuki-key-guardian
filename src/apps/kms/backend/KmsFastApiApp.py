045894f765609c7928ed75660c9505fbb2902ec9
Javier Parada <javierparada73@outlook.com>
2022-03-31 18:59:53 +0200

    improve docs with multiple servers

--- src/apps/kms/backend/KmsFastApiApp.py
+++ src/apps/kms/backend/KmsFastApiApp.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.openapi.utils import get_openapi
 from starlette.middleware.cors import CORSMiddleware
 
 from src.apps.kms.backend.routes import register_routes
@@ -7,13 +8,7 @@ from src.apps.kms.backend.routes import register_routes
 class KmsFastApiApp:
 
     def __init__(self):
-        self._app: FastAPI = FastAPI(
-            title="Tanuki Key Guardian - KMS",
-            license_info={
-                'url': 'https://www.apache.org/licenses/LICENSE-2.0.html',
-                'name': 'Apache 2.0'
-            }
-        )
+        self._app: FastAPI = FastAPI()
 
         self._app.add_middleware(
             CORSMiddleware,
@@ -26,6 +21,36 @@ class KmsFastApiApp:
         router: APIRouter = APIRouter()
         register_routes(router)
         self._app.include_router(router, prefix='/api')
+
+    def custom_openapi(self, current_host=None, current_port=None):
+        if self._app.openapi_schema:
+            return self._app.openapi_schema
+        openapi_schema = get_openapi(
+            title="Tanuki Key Guardian - KMS",
+            version="1.0.0",
+            routes=self._app.routes,
+            license_info={
+                'url': 'https://www.apache.org/licenses/LICENSE-2.0.html',
+                'name': 'Apache 2.0'
+            }
+        )
+        servers = []
+        if current_host and current_port:
+            servers.append({
+                'url': f'{current_host}:{current_port}',
+                'description': 'Localhost'
+            })
+
+        # servers.extend([
+        #     {
+        #         'url': 'any_url.com',
+        #         'description': 'static_sample'
+        #     }
+        # ])
+
+        openapi_schema['servers'] = servers
+        self._app.openapi_schema = openapi_schema
+        return self._app.openapi_schema
 
     def get_runnable(self):
         return self._app
--- src/apps/kms/backend/KmsHttpServer.py
+++ src/apps/kms/backend/KmsHttpServer.py
@@ -13,4 +13,5 @@ class KmsHttpServer:
     def run(self):
         host = EnvManager.get(EnvVar.KMS_SERVER_HOST)
         port = EnvManager.get(EnvVar.KMS_SERVER_PORT, parser=int)
+        self.app.custom_openapi(host, port)
         uvicorn.run(self.app.get_runnable(), host=host, port=port)
