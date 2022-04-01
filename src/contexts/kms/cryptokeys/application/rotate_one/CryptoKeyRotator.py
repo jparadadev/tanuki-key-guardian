1ec4d6105d334a144b66f20ff30918615936310e
Javier Parada <javierparada73@outlook.com>
2022-04-01 17:48:52 +0200

    fix key rotation

--- src/contexts/kms/cryptokeys/application/rotate_one/CryptoKeyRotator.py
+++ src/contexts/kms/cryptokeys/application/rotate_one/CryptoKeyRotator.py
@@ -2,7 +2,7 @@ from src.contexts.kms.cryptokeys.domain.entities.CryptoKeyId import CryptoKeyId
 from src.contexts.kms.cryptokeys.domain.entities.CryptoKeyPayload import CryptoKeyPayload
 from src.contexts.kms.cryptokeys.domain.repositories.CryptoKeyRepository import CryptoKeyRepository
 from src.contexts.shared.domain.EventBus import EventBus
-
+import random
 
 class CryptoKeyRotator:
 
@@ -12,9 +12,17 @@ class CryptoKeyRotator:
 
     async def run(self, key_id: CryptoKeyId):
         crypto_key = await self._cryptokey_repository.find_by_id(key_id)
+        chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        length = 62
+
+        def uid(seed):
+            random.seed(seed)
+
+            return ''.join([chars[int(random.random() * length)] for _ in range(len(crypto_key.payload.value()))])
 
         # TODO: rotation
-        crypto_key.payload = CryptoKeyPayload(f'{crypto_key.payload.value()}+')
-
+        uid(crypto_key.payload.value())
+        rote = uid(crypto_key.payload.value())
+        crypto_key.payload= CryptoKeyPayload(rote)
         await self._cryptokey_repository.update_one(crypto_key)
         # TODO: dispatch key rotated event
