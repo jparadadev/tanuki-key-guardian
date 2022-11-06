4e0b5cb72b7c2269169d111c097c09c5bdb18c39
Javier Parada <javierparadadev@gmail.com>
2022-11-06 18:47:25 +0100

    remove unused vars

--- src/contexts/shared/Infrastructure/environment/EnvVar.py
+++ src/contexts/shared/Infrastructure/environment/EnvVar.py
@@ -14,9 +14,6 @@ class EnvVar(Enum):
     KMS_CLIENT_MONGO_HOST = 'kms.client.mongo.host'
     KMS_CLIENT_MONGO_PORT = 'kms.client.mongo.port'
 
-    KMS_CRYPTOKEY_MONGO_HOST = 'kms.cryptokey.mongo.host'
-    KMS_CRYPTOKEY_MONGO_PORT = 'kms.cryptokey.mongo.port'
-
     # -------------------------------------------------------
     # ---------------------- SHARED -------------------------
     # -------------------------------------------------------
