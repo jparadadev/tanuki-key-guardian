549c6c94ee87e3857f28d6eb612c74483153acef
Javier Parada <javierparadadev@gmail.com>
2022-12-01 18:03:12 +0100

    add default port and host

--- src/apps/kms/backend/KmsHttpServer.py
+++ src/apps/kms/backend/KmsHttpServer.py
@@ -11,7 +11,7 @@ class KmsHttpServer:
         self.app = KmsFastApiApp()
 
     def run(self):
-        host = EnvManager.get(EnvVar.KMS_SERVER_HOST)
-        port = EnvManager.get(EnvVar.KMS_SERVER_PORT, parser=int)
+        host = EnvManager.get(EnvVar.KMS_SERVER_HOST) or '0.0.0.0'
+        port = EnvManager.get(EnvVar.KMS_SERVER_PORT, parser=int) or 80
         self.app.custom_openapi(host, port)
         uvicorn.run(self.app.get_runnable(), host=host, port=port)
